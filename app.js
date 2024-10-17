15708e8823b01b87303e667e8334c394e3fe5985
unknown <maima.alive25@gmail.com>
2024-10-17 22:03:08 +0500

    my first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+/*do while loop
+ syntax  let  do{}while()*/
+var a = 4;
+do {
+    console.log(a);
+    a++;
+} while (a < 5);
+var i = 1;
+do {
+    console.log(2 + 3, i);
+    i++;
+} while (i < 6);
---
+++ app.ts
@@ -0,0 +1,13 @@
+/*do while loop
+ syntax  let  do{}while()*/
+ let a=4;
+ do{
+   console.log(a)
+   a++
+ }while(a<5)
+
+     let i=1
+     do{
+        console.log(2+3,i)
+        i++
+     } while(i<6)
